5a39afdd02e324a278a81cfeffe1c586802636af
all-you-need-is-laugh <smerost2@gmail.com>
2022-03-27 21:32:05 +0200

    Moved: bootstrap logic related to `AppModule` to its static method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
   imports: [ MessagesModule ],
 })
-export class AppModule {}
+export class AppModule {
+  // TODO: set TS config setting "noImplicitAny": true,
+  static async bootstrap (app) {
+    app.useGlobalPipes(
+      new ValidationPipe({
+        forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: { enableImplicitConversion: true },
+        whitelist: true,
+      }),
+    );
+
+    return app;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: { enableImplicitConversion: true },
-      whitelist: true,
-    }),
-  );
+  AppModule.bootstrap(app);
 
   // TODO: add configuration
   await app.listen(3000);
